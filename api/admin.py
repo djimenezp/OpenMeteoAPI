c5c22d063b90ba87ea0c4d1cc9a2744ecb189bfc
David Jimenez Pérez <d900929.jp@gmail.com>
2026-02-07 18:48:59 +0100

    Register admin configurations for City, WeatherDataset, and WeatherHour models

---
+++ api/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from .models import City, WeatherDataset, WeatherHour
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "country_code", "country", "timezone", "latitude", "longitude")
+    search_fields = ("name", "country", "country_code")
+    list_filter = ("country_code", "timezone")
+    ordering = ("name", "country_code")
+    readonly_fields = ("data",)  # JSON can be big
+
+
+@admin.register(WeatherDataset)
+class WeatherDatasetAdmin(admin.ModelAdmin):
+    list_display = ("id", "city", "start_date", "end_date", "source", "created_at", "hours_count")
+    search_fields = ("city__name", "city__country", "city__country_code")
+    list_filter = ("city__country_code", "source", "start_date", "end_date")
+    ordering = ("-created_at",)
+
+    def hours_count(self, obj):
+        return obj.hours.count()
+    hours_count.short_description = "hours"
+
+
+@admin.register(WeatherHour)
+class WeatherHourAdmin(admin.ModelAdmin):
+    list_display = ("id", "dataset", "timestamp", "temperature", "precipitation")
+    search_fields = ("dataset__city__name", "dataset__city__country_code")
+    list_filter = ("dataset__city__country_code", "dataset__city__name")
+    ordering = ("-timestamp",)
+    date_hierarchy = "timestamp"
